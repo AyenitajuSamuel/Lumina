ae9641b37555f6b1fac6c4b133ba7de0a68d02cc
Samuel <samuelayenitaju@gmail.com>
2026-01-26 17:21:50 +0100

    Switch backend to django

---
+++ Lumina/blog/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django_ckeditor_5.fields import CKEditor5Field
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    excerpt = models.CharField(max_length=300, default='Post excerpt')
+    content = CKEditor5Field()
+    date_posted = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='images/', blank=True, null=True)
+
+    def __str__(self):
+        return self.title
